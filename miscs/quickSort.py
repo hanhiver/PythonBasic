7a1ef22076fc938ddb169e8ea97ac6df3269b20d
Dong Han <handongfr@gmail.com>
2018-10-29 20:20:42 +0800

    Add quicksort and decoratortest

---
+++ miscs/quickSort.py
@@ -0,0 +1,20 @@
+""" Python version of quick sort. 
+	Dong Han 2018.10.29 for fun. """
+
+def quickSort(array):
+
+	if (len(array) <= 1):
+		return array
+
+	midValue = array[ (len(array) // 2) ]
+
+	left = [ x for x in array if x < midValue ]
+	middle = [ x for x in array if x == midValue ]
+	right = [ x for x in array if x > midValue ]
+
+	return quickSort(left) + middle + quickSort(right)
+
+if __name__ == '__main__':
+	array = [4, 5, 8, 0, 1, 4, 6, 7, 10, 45, 3, -2, -8]
+	print(quickSort(array))
+
