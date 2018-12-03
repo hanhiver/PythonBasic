cb6f9f29f23358bb1d9e864c44bdcc29a2a84869
Dong Han <handongfr@gmail.com>
2018-12-03 12:16:53 +0800

    Add multiprocess example

---
+++ multiProcess/multiProcessPool.py
@@ -0,0 +1,26 @@
+from multiprocessing import Pool
+import os, time, random
+
+def long_time_task(name):
+	print('Run task {} ({})...'.format(name, os.getpid()))
+	start = time.time()
+	time.sleep(random.random())
+	end = time.time()
+	print('Task {} run {:2f} seconds.'.format(name, (end - start)))
+
+if __name__ == '__main__':
+	print('Parent process {}.'.format(os.getpid()))
+
+	p = Pool(4)
+	#p = Pool()
+
+	for i in range(10):
+		p.apply_async(long_time_task, args=(i, ))
+
+	print('Waiting for all child processes done...')
+
+	p.close()
+	p.join()
+
+	print('All child processes done. ')
+
