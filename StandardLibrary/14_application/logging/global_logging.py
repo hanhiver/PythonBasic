1d851238f26d64796720a9370a202a20ae3aeb0a
Dong Han <handongfr@gmail.com>
2019-01-31 15:12:19 +0800

    add loggerManager.py

--- StandardLibrary/14_application/logging/global_logging.py
+++ StandardLibrary/14_application/logging/global_logging.py
@@ -16,9 +16,9 @@ import logging.handlers
 import time
 import glob
 
-########################
-# Local Logging system #
-########################
+############################
+### Local Logging system ###
+############################
 
 class SingleProcessLogger():
 
@@ -27,5 +27,31 @@ class SingleProcessLogger():
 				 max_bytes = 500, 
 				 backup_count = 5):
 
-		self.logger = logging.getLogger('')
+		self.file_handler = logging.handlers.RotatingFileHandler(
+							filename, maxBytes = max_bytes, backupCount = backup_count)
+		self.stream_handler = logging.StreamHandler()
+		self.formatter = logging.Formatter('%(asctime)s <%(threadName)s>: %(message)s')
+
+	def get_logger(module, 
+				   log_level = logging.WARNING, 
+				   log_formatter = self.formatter):
+
+		# Get a default Logger. 
+		new_logger = logging.getLogger(module)
+
+		# Associate the logger to file_handler and stream_handler. 
+		new_logger.addHandler(self.file_handler)
+		new_logger.addHandler(self.stream_handler)
+
+		# Associate the default formatter to the logger. 
+		new_logger.setFormatter(log_formatter)
+
+######################################
+### Multi-Processes Logging system ###
+######################################
+
+
+
+
+				
 
---
+++ general_logging/test.log.dhans-mbp.cn.ibm.com
@@ -0,0 +1,5 @@
+2019-01-31 14:06:06,928    INFO  <67124:MainThread> - [write_log.write_log] - test.py[line:13] A INFO information in thread. 
+2019-01-31 14:06:06,927   DEBUG  <67125:MainThread> - [write_log.write_log] - test.py[line:12] A DEBUG infomation in thread. 
+2019-01-31 14:06:06,930    INFO  <67125:MainThread> - [write_log.write_log] - test.py[line:13] A INFO information in thread. 
+2019-01-31 14:06:06,930   DEBUG  <67126:MainThread> - [write_log.write_log] - test.py[line:12] A DEBUG infomation in thread. 
+2019-01-31 14:06:06,933    INFO  <67126:MainThread> - [write_log.write_log] - test.py[line:13] A INFO information in thread. 
