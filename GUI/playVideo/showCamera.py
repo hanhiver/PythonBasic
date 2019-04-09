48ee3948f4d21984b2fbc1cc39533bab94237189
hanhiver <13910395157@139.com>
2019-04-09 15:13:59 +0800

    update qt example showcamero

--- GUI/playVideo/showCamera.py
+++ GUI/playVideo/showCamera.py
@@ -22,32 +22,33 @@ windows.geometry('%dx%d+%d+%d' % (wx, wh, (sw-wx)/2, (sh-wh)/2-100))
 
 # Draw the canvas. 
 canvas = tk.Canvas(windows, bg = '#c4c2c2', 
-	               height = wh, width = wx)
+                   height = wh, width = wx)
 canvas.pack()
 
 # Open the camera to get images. 
 ################################
 def video_demo():
-	capture = cv2.VideoCapture(0)
+    #capture = cv2.VideoCapture(0)
+    capture = cv2.VideoCapture('/home/dhan/myprog/wsdata/20190405/无滤光无衰减/手动调焦自动曝光.avi')
 
-	def cc(capture):
-		#capture = cv2.VideoCapture(0)
-		while True:
-			ret, frame = capture.read()
-			frame = cv2.flip(frame, 1)
-			cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-			img = Image.fromarray(cv2image)
-			image_file = ImageTk.PhotoImage(img)
-			canvas.create_image(0, 0, anchor = 'nw', image = image_file)
-	
-	t = threading.Thread(target = cc, args = (capture, ))
-	t.start()
+    def cc(capture):
+        #capture = cv2.VideoCapture(0)
+        while True:
+            ret, frame = capture.read()
+            frame = cv2.flip(frame, 1)
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+            img = Image.fromarray(cv2image)
+            image_file = ImageTk.PhotoImage(img)
+            canvas.create_image(0, 0, anchor = 'nw', image = image_file)
+    
+    t = threading.Thread(target = cc, args = (capture, ))
+    t.start()
 
 # Define a botton to start the image show. 
 ##########################################
 bt_start = tk.Button(windows, text = 'Open Camera', 
-	                 height = 2, width = 15, 
-	                 command = video_demo)
+                     height = 2, width = 15, 
+                     command = video_demo)
 bt_start.place(x = 230, y = 600)
 tk.mainloop()
 
@@ -55,9 +56,9 @@ import sys
 import time
  
 for i in range(10000):
-	percent = 1.0 * i / 10000 * 100
-	print('complete percent:' + str(percent) + ‘%’, sys.stdout.write('\r'))
-   	time.sleep(0.01)
+    percent = 1.0 * i / 10000 * 100
+    print('complete percent:' + str(percent) + '%', sys.stdout.write('\r'))
+    time.sleep(0.001)
 
 
 
