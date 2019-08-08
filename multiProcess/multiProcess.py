212bef5a7b4c95607e70f4d03dcecf37ff9727c6
hanhiver <13910395157@139.com>
2019-08-08 20:14:11 +0800

    update

--- multiProcess/multiProcess.py
+++ multiProcess/multiProcess.py
@@ -11,13 +11,19 @@ if __name__ == '__main__':
 	#print('父进程 {}.'.format(os.getpid()))
 	print('Father process: {}.'.format(os.getpid()))
 
-	p = Process(target = run_proc, args = ('test', ))
+	p1 = Process(target = run_proc, args = ('test', ))
+	p2 = Process(target = run_proc, args = ('test', ))
+	p3 = Process(target = run_proc, args = ('test', ))
 
 	print('Starting the child process.')
 	#print('子进程将要启动。')
 
-	p.start()
-	p.join()
+	p1.start()
+	p2.start()
+	p3.start()
+	p1.join()
+	p2.join()
+	p3.join()
 
 	#print('子进程结束。')
 	print('Child process ended.')
