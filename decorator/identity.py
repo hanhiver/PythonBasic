658121628273588c2a5dd6e77ff6eda83ab49e58
hanhiver <13910395157@139.com>
2019-08-12 10:32:09 +0800

    update

---
+++ decorator/identity.py
@@ -0,0 +1,14 @@
+def identity(f):
+    return f
+
+@identity
+def foo():
+    return 'bar'
+
+def main():
+    print(foo())
+
+if __name__ == '__main__':
+    main()
+
+
--- yaml/test.py
+++ yaml/test.py
@@ -7,7 +7,7 @@ print(yaml.__version__)
 f = open(sys.argv[1], 'r')
 cont = f.read()
 
-x = yaml.load(cont, Loader = yaml.FullLoader)
-#x = yaml.safe_load(cont)
-print(x)
+x = yaml.load(cont, Loader=yaml.FullLoader)
 
+# x = yaml.safe_load(cont)
+print(x)
