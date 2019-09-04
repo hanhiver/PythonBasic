05ec05e71e630e7a3c310a094ce289db2e104a52
hanhiver <13910395157@139.com>
2019-09-04 17:45:30 +0800

    update

--- python.so/multi-thread-so/classWithStatus/norm.cpp
+++ python.so/multi-thread-so/classWithStatus/norm.cpp
@@ -47,7 +47,7 @@ public:
         // (1) drop the input
         // (2) add drop value to he drop_vect
         // (3) return the previous value. 
-        if delta > thres_drop:
+        if (delta > thres_drop)
         {
             cout << "DROP: " << input << endl; 
             drop_vect.push_back(input);
@@ -57,12 +57,35 @@ public:
             if (drop_vect.size() > dlen)
             {
                 cout << "TRACK LOST! Re-catch it!" << endl;
-                value_vect = std::vector(drop_vect);
-                drop_vect.release();
+                value_vect = std::move(drop_vect);
+                drop_vect.clear();
             }
 
-        
+            return avg;
+        }
 
+        // if the delta lager than thres_norm,
+        // (1) add to value_vect
+        // (2) output normalized value
+        if (delta > thres_norm)
+        {
+            cout << "OVERED: " << input << endl;
+            // Add the input value to the value_vect. 
+            value_vect.push_back(input);
+            input = (avg * 2 + input) / 3;
+        }
+
+        // Check if the value is in the move_limit range, 
+        // if the change is less than move_limit, will not change the output.
+        int pre_value = value_vect[value_vect.size()-2];
+        if abs(input - pre_value) < move_limit:
+        {
+            input = pre_value;
+        }
+
+        drop_vect.clear();
+
+        return input;
     }
 
     void printValue()
