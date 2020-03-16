8f79130a42ee61cb8aa8b430ea3d20e34831a2c2
hanhiver <handongfr@163.com>
2020-03-17 00:07:50 +0800

    update

--- yaml/test.py
+++ yaml/test.py
@@ -4,10 +4,18 @@ import sys
 print("Hello, YAML!")
 print(yaml.__version__)
 
-f = open(sys.argv[1], 'r')
+#f = open(sys.argv[1], 'r')
+f = open('./test.yaml', 'r')
 cont = f.read()
+f.close()
 
 x = yaml.load(cont, Loader=yaml.FullLoader)
+x['image'] = "MYIMAGE"
 
 # x = yaml.safe_load(cont)
 print(x)
+
+with open('./out.yaml', 'w',  encoding='utf-8') as file: 
+    yaml.dump(x, file)
+
+
