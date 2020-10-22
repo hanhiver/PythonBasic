325425c54473c769de37697d95f767fa3f3c213c
hanhiver <handongfr@163.com>
2020-10-22 13:49:59 +0800

    update

--- alibaba/oss/object_basic.py
+++ alibaba/oss/object_basic.py
@@ -1,10 +1,7 @@
 import oss2 
 from itertools import islice
 
-# AccessKey ID: LTAI4GJRZRRjpURD7nAerLwd
-# AccessKey Secret: QxW133A6UP7QC4avCxvgG7xsu7ddjh
-
-auth = oss2.Auth('LTAI4GJRZRRjpURD7nAerLwd', 'QxW133A6UP7QC4avCxvgG7xsu7ddjh')
+auth = oss2.Auth('AK'', 'ASK')
 bucket = oss2.Bucket(auth, 'http://oss-cn-beijing.aliyuncs.com', 'dhan-test')
 
 for b in islice(oss2.ObjectIterator(bucket), 10):
