a2ff6a4ad50e365ae8d392912537320e99235e8f
hanhiver <handongfr@163.com>
2020-11-02 14:19:07 +0800

    update

--- alibaba/oss/object_basic.py
+++
@@ -1,8 +0,0 @@
-import oss2 
-from itertools import islice
-
-auth = oss2.Auth('AK'', 'ASK')
-bucket = oss2.Bucket(auth, 'http://oss-cn-beijing.aliyuncs.com', 'dhan-test')
-
-for b in islice(oss2.ObjectIterator(bucket), 10):
-    print(b.key)
